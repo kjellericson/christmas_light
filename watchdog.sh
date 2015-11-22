146f5a1a8f8fdb3c33e565438f46d8cab31b484c
Kjell Ericson <kjell@haxx.se>
2015-11-22 17:11:33 +0100

    Added a watchdog.

---
+++ watchdog.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Keep light awake
+# Also adding time check and run certain times.
+
+while true
+do
+  now=$(date +"%H%M") 
+  date
+  echo "Now = $now"
+  if [ $now -ge 1500 -a $now -le 2359 ]
+  then 
+    sudo ./christmas_light
+  elif [ $now -ge 0400 -a $now -le 0830 ]
+  then 
+    sudo ./christmas_light
+  else
+    sleep 10
+  fi
+  sleep 2
+done
