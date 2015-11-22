a053f05c44587a20076e9e170ffc37ea3a38b4c1
Kjell Ericson <kjell@haxx.se>
2015-11-22 19:18:01 +0100

    Seperated watchdog and timer controller.

--- watchdog.sh
+++ watchdog.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
 # Keep light awake
-# Also adding time check and run certain times.
 
 while true
 do
-  now=$(date +"%H%M") 
-  date
-  echo "Now = $now"
-  if [ $now -ge 1500 -a $now -le 2359 ]
-  then 
-    sudo ./christmas_light
-  elif [ $now -ge 0400 -a $now -le 0830 ]
-  then 
-    sudo ./christmas_light
-  else
-    sleep 10
-  fi
-  sleep 2
+  ./timercontrol.sh
+
+# Wait between run
+  sleep 5
 done
