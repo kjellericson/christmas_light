c70c69df2d4f09336fb874ce6fab2dbc6f197f17
Kjell Ericson <kjell@haxx.se>
2015-11-28 20:46:43 +0100

    Added a file control to turn light on.

--- timercontrol.sh
+++ timercontrol.sh
@@ -9,10 +9,28 @@ echo $$ >timercontrol.pid
 # Run forever
 while true
 do
+  ##
+  ## Get time
+  ##
   now=$(date +"%H%M") 
   date
   echo "Now = $now"
-  if [ $now -ge 1500 -a $now -le 2359 ]
+
+  ##
+  ## Check file turn_on.txt.  If it contains 1 then we turn light on.
+  ##
+  turn_on = 0
+  if [ -f "turn_on.txt" ]
+  then
+    turn_on = `cat turn_on.txt`;
+  fi
+
+  ##
+  ## Decide if we shall run christmas light
+  if [ $turn_on -eq "1" ]
+  then
+    sudo ./christmas_light
+  elif [ $now -ge 1500 -a $now -le 2359 ]
   then 
     sudo ./christmas_light
   elif [ $now -ge 0400 -a $now -le 0830 ]
